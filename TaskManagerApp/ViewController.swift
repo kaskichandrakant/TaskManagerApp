7abf2dee287d31a4775fb342d69d2d22c433b5fd
kaskichandrakant <kaskichandrakant@gmail.com>
2018-05-30 23:34:53 +0530

    added basic table view and random values

--- TaskManagerApp/ViewController.swift
+++ TaskManagerApp/ViewController.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    let cellId = "cellId"
+    var items: [String] = ["some thing" , "some another thing"]
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         navigationController?.navigationBar.barTintColor = .red
         navigationController?.navigationBar.tintColor = .white
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "add", style: .plain, target: self, action: #selector(addItem))
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
     
     @objc func addItem(_ sender: AnyObject){
         print("added")
     }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item
+        return cell
+    }
+    
 }
 
