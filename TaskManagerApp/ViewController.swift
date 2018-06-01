75c2c795dc66f4e7cc3b133b2a9cd9bfeb422946
kaskichandrakant <kaskichandrakant@gmail.com>
2018-06-01 13:03:20 +0530

    removed the data saving part

--- TaskManagerApp/ViewController.swift
+++ TaskManagerApp/ViewController.swift
@@ -7,67 +7,50 @@
 //
 
 import UIKit
-import CoreData
 
 class ViewController: UITableViewController {
     let cellId = "cellId"
-    var items: [NSManagedObject] = []
-    
+    var items: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         navigationController?.navigationBar.barTintColor = .red
         navigationController?.navigationBar.tintColor = .white
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addItem))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "add", style: .plain, target: self, action: #selector(addItem))
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
     
     @objc func addItem(_ sender: AnyObject){
-        let alertController = UIAlertController(title: "Add new item", message: "write task to add", preferredStyle: .alert)
+        let alertController = getAlertController()
         let saveAction = UIAlertAction(title: "save", style: .default) {[unowned self] action in
-            guard let textField = alertController.textFields?.first , let itemToAdd = textField.text else {return}
-            self.save(itemToAdd)
+            guard let textField = alertController.textFields?.first , let item = textField.text else {return}
+            self.add(itemToAdd: item)
             self.tableView.reloadData()
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
         alertController.addTextField(configurationHandler: nil)
         alertController.addAction(saveAction)
-        alertController.addAction(cancelAction)
         present(alertController, animated: true,completion: nil)
     }
+    
+    func getAlertController()-> UIAlertController{
+        let alertController = UIAlertController(title: "Add new item", message: "write task to add", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alertController.addAction(cancelAction)
+        return alertController
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
-    override func viewWillAppear(_ animated: Bool) {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let fetchReq = NSFetchRequest<NSManagedObject>(entityName: "Item")
-        do {
-            items = try managedContext.fetch(fetchReq)
-        } catch let err as NSError {
-            print("fetch error",err)
-        }
-        
-    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         let item = items[indexPath.row]
-        cell.textLabel?.text = item.value(forKeyPath: "itemName") as? String
+        cell.textLabel?.text = item
         return cell
     }
-    
-    func save(_ itemName: String){
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
-        let manageContext = appDelegate.persistentContainer.viewContext
-        let entity = NSEntityDescription.entity(forEntityName: "Item", in: manageContext)!
-        let item = NSManagedObject(entity: entity, insertInto:  manageContext)
-        do {
-            try manageContext.save()
-            items.append(item)
-        } catch let err as NSError {
-            print("save err",err)
-        }
-        item.setValue(itemName , forKey: "itemName")
+    func add(itemToAdd: String){
+        items.append(itemToAdd)
     }
 }
 
